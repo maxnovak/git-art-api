83f4de0f1650350ed6dffe17986a8635f6f7069c
maxnovak <novak.max@gmail.com>
2020-02-22 12:27:29 -0500

    Extract letter parsing into own method

--- src/designs/word.go
+++ src/designs/word.go
@@ -13,65 +13,20 @@ import (
 
 // DrawWord creates a git history based on the passed in word
 func DrawWord(word string, date time.Time) {
+	date = helpers.AddDays(date, 1)
 	var characters = []rune(strings.ToLower(word))
 	for _, character := range characters {
-		if character == 'a' {
-			data, err := ioutil.ReadFile("../src/images/a.json")
-			if err != nil {
-				log.Fatal(err)
-			}
-			date = drawLetter(date, data)
-		}
-		if character == 'b' {
-			data, err := ioutil.ReadFile("../src/images/b.json")
-			if err != nil {
-				log.Fatal(err)
-			}
-			date = drawLetter(date, data)
-		}
-		if character == 'c' {
-			data, err := ioutil.ReadFile("../src/images/c.json")
-			if err != nil {
-				log.Fatal(err)
-			}
-			date = drawLetter(date, data)
-		}
-		if character == 'd' {
-			data, err := ioutil.ReadFile("../src/images/d.json")
-			if err != nil {
-				log.Fatal(err)
-			}
-			date = drawLetter(date, data)
-		}
-		if character == 'e' {
-			data, err := ioutil.ReadFile("../src/images/e.json")
-			if err != nil {
-				log.Fatal(err)
-			}
-			date = drawLetter(date, data)
-		}
-		if character == 'f' {
-			data, err := ioutil.ReadFile("../src/images/f.json")
-			if err != nil {
-				log.Fatal(err)
-			}
-			date = drawLetter(date, data)
-		}
-		if character == 'g' {
-			data, err := ioutil.ReadFile("../src/images/g.json")
-			if err != nil {
-				log.Fatal(err)
-			}
-			date = drawLetter(date, data)
-		}
-		if character == 'h' {
-			data, err := ioutil.ReadFile("../src/images/h.json")
-			if err != nil {
-				log.Fatal(err)
-			}
-			date = drawLetter(date, data)
-		}
+		data := getLetter(character)
+		date = drawLetter(date, data)
 	}
+}
+
+func getLetter(character rune) []byte {
+	data, err := ioutil.ReadFile(fmt.Sprintf("../src/images/%v.json", string(character)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
 }
 
 func drawLetter(date time.Time, data []byte) time.Time {
