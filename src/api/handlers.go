5b5feec8efa2cd6acff5f7db8d8c8d35ea561111
Maxwell Novak <novak.max@gmail.com>
2020-05-13 11:04:13 -0400

    Create new endpoint that errors in json is invalid

--- src/api/handlers.go
+++ src/api/handlers.go
@@ -1,6 +1,11 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // Request object that api accepts for generating git pattern
 type Request struct {
@@ -17,4 +22,26 @@ func Ping(context *gin.Context) {
 	context.JSON(200, gin.H{
 		"message": "pong",
 	})
+}
+
+// MakeArt accepts the json request
+func MakeArt(context *gin.Context) {
+	var json Request
+	if err := context.ShouldBindJSON(&json); err != nil {
+		if err.Error() == "EOF" {
+			context.JSON(
+				http.StatusBadRequest,
+				gin.H{"error": "Empty Payload"},
+			)
+			return
+		}
+
+		context.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	log.Printf("Request: %+v\n", json)
 }
--- src/api/rest.go
+++ src/api/rest.go
@@ -10,6 +10,7 @@ import (
 func RestAPI() {
 	router := gin.Default()
 	router.GET("/ping", Ping)
+	router.POST("/make-art", MakeArt)
 
 	router.Run() // listen and serve on localhost:8080
 	log.Print("Server Started")
