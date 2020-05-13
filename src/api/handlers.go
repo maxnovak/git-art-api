7470c486d6e7adb6e724f3d1c2cb44d0539c4f32
Maxwell Novak <novak.max@gmail.com>
2020-05-13 10:51:43 -0400

    Create shape of request object

--- src/api/handlers.go
+++ src/api/handlers.go
@@ -2,6 +2,16 @@ package api
 
 import "github.com/gin-gonic/gin"
 
+// Request object that api accepts for generating git pattern
+type Request struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Pattern  string `json:"pattern" binding:"required"`
+	Word     string `json:"word"`
+	Year     int    `json:"year" binding:"required"`
+	RepoName string `json:"repoName" binding:"required"`
+}
+
 // Ping for ping/pong default endpoint
 func Ping(context *gin.Context) {
 	context.JSON(200, gin.H{
