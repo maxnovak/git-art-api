5e53503efddce93f806b1aaaffe379b400ce6541
Maxwell Novak <novak.max@gmail.com>
2020-05-13 10:47:46 -0400

    Rename variables to be a little more readable

--- src/api/handlers.go
+++ src/api/handlers.go
@@ -2,9 +2,9 @@ package api
 
 import "github.com/gin-gonic/gin"
 
-// Handler for ping/pong default endpoint
-func Handler(c *gin.Context) {
-	c.JSON(200, gin.H{
+// Ping for ping/pong default endpoint
+func Ping(context *gin.Context) {
+	context.JSON(200, gin.H{
 		"message": "pong",
 	})
 }
