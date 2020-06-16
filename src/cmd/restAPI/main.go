d88b959b945f19fe2fb74ac45a078ad18efc06d5
Maxwell Novak <novak.max@gmail.com>
2020-06-15 20:11:19 -0400

    Specify port

--- src/cmd/restAPI/main.go
+++ src/cmd/restAPI/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"git-art/src/api"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +14,6 @@ func main() {
 	router.GET("/ping", api.Ping)
 	router.POST("/make-art", api.MakeArt)
 
-	router.Run() // listen and serve on localhost:8080
+	router.Run(":" + os.Getenv("PORT")) // listen and serve on localhost:8080
 	log.Print("Server Started")
 }
